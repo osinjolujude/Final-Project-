8dfcb118d1fbeefdac3fcb665ad5b089f5002344
osinjolujude <osinjolujude@yahoo.co.uk>
2022-08-30 15:14:27 -0600

    Modified index file
    Added HTML file

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
 Hello dear
 
-This is a test file from me Mayowa Osinjolu
+This is a test file from __future__ import annotations
+from me Mayowa Osinjolu
+
+
+annotations __build_class__ edited by Jude
